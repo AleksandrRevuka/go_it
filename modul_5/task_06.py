8efa34716f07a4df2bc42cd77cc394cf2676900c
alex <Asmodians@hotmail.com>
2023-03-10 19:18:11 +0200

    task_06

---
+++ modul_5/task_06.py
@@ -0,0 +1,39 @@
+# def is_spam_words(text, spam_words, space_around=False):
+#     for spam_word in spam_words:
+#         if spam_word.lower() in text.lower():
+#             if space_around is False:
+#                 return True
+#             elif space_around is True:
+#                 for word in text.split(' '):
+#                     word = word.strip('., ,,')
+#                     if word == spam_word:
+#                         return True
+#                     else:
+#                         continue
+#
+#     return False
+
+import re
+
+
+def is_spam_words(text, spam_words, space_around=False):
+    """GPT_chat"""
+    text = text.lower()
+
+    for spam_word in spam_words:
+        if space_around:
+            pattern = r'(^|\s)' + re.escape(spam_word) + r'($|\s|\.\,)'
+            if re.search(pattern, text):
+                return True
+        else:
+            if spam_word in text:
+                return True
+
+    return False
+
+
+print(is_spam_words("Молох Передбачити третій параметр функції ", ["лох", "функції"]))
+print(is_spam_words("Молох Передбачити третій параметр функції ", ["лох", "функції_"], True))
+
+print(is_spam_words("Ты лох. Передбачити третій параметр функції ", ["лох", "функції"]))
+print(is_spam_words("Ты лох, Передбачити третій параметр функції ", ["лох", "функції"], True))
