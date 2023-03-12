313b4cf71fe158eaf9b6691947d1d4af4dbb11ed
alex <Asmodians@hotmail.com>
2023-03-12 12:52:07 +0200

    task_07

---
+++ modul_5/task_07.py
@@ -0,0 +1,29 @@
+CYRILLIC_SYMBOLS = [ord(symbol) for symbol in "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"]
+TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
+               "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g")
+
+TRANS = dict(zip(CYRILLIC_SYMBOLS, TRANSLATION))
+
+
+def translate(name):
+    return name.translate(TRANS)
+
+
+print(translate("Дмитро Короб"))  # Dmitro Korob
+print(translate("Олекса Івасюк"))  # Oleksa Ivasyuk
+
+
+##############################################################################################
+
+# CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
+# TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
+#                "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g")
+#
+# TRANS = {}
+# for c, l in zip(CYRILLIC_SYMBOLS, TRANSLATION):
+#     TRANS[ord(c)] = l
+#     TRANS[ord(c.upper())] = l.upper()
+#
+#
+# def translate(name):
+#     return name.translate(TRANS)
