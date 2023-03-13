999b77e65e5895f9cdf322151fd62410491b3d1e
AleksandrRevuka <113589337+AleksandrRevuka@users.noreply.github.com>
2023-03-13 22:33:40 +0200

    task_10

--- modul_5/task_09.py
+++ modul_5/task_09.py
@@ -1,4 +1,4 @@
-def formatted_numbers():
+def formatted_numbers() -> list:
     """formatted_numbers"""
     header = "|{:^9} |{:^9} |{:^10}|".format('decimal', 'hex', 'binary')
     result = ["|{0:<9d} |{0:^9x} |{0:>10b}|".format(number) for number in range(16)]
---
+++ modul_5/task_10.py
@@ -0,0 +1,22 @@
+import re
+
+
+def find_word(text: str, word: str) -> dict:
+    """find word"""
+    result = False
+    result_find = re.search(word, text)
+    if result_find:
+        result = True
+        index = result_find.span()
+    else:
+        index = (None, None)
+    return {
+            'result': result,
+            'first_index': index[0],
+            'last_index': index[1],
+            'search_string': word,
+            'string': text
+            }
+
+
+print(find_word("Guido van Rossum began working on Python in the late 1980s, as a successor to the ABC programming language, and first released it in 1991 as Python 0.9.0.", "Sasha"))
