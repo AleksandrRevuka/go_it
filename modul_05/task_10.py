52c7be830d7d7e546383cb7d436d0a6b80e1d62c
AleksandrRevuka <113589337+AleksandrRevuka@users.noreply.github.com>
2023-03-13 22:47:33 +0200

    task_10_1

--- modul_05/task_10.py
+++ modul_05/task_10.py
@@ -1,3 +1,4 @@
+"""Program"""
 import re
 
 
@@ -11,12 +12,12 @@ def find_word(text: str, word: str) -> dict:
     else:
         index = (None, None)
     return {
-            'result': result,
-            'first_index': index[0],
-            'last_index': index[1],
-            'search_string': word,
-            'string': text
-            }
+        'result': result,
+        'first_index': index[0],
+        'last_index': index[1],
+        'search_string': word,
+        'string': text
+    }
 
 
 print(find_word("Guido van Rossum began working on Python in the late 1980s, as a successor to the ABC programming language, and first released it in 1991 as Python 0.9.0.", "Sasha"))
