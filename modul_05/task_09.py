da6e9f5226989de9b30a3e1d2d938a9c3e5b1906
AleksandrRevuka <113589337+AleksandrRevuka@users.noreply.github.com>
2023-03-15 09:33:47 +0200

    task_09

--- modul_05/task_09.py
+++ modul_05/task_09.py
@@ -4,8 +4,7 @@
 def formatted_numbers() -> list:
     """formatted_numbers"""
     header = "|{:^9} |{:^9} |{:^10}|".format('decimal', 'hex', 'binary')
-    result = ["|{0:<9d} |{0:^9x} |{0:>10b}|".format(number)
-              for number in range(1, 15)]
+    result = ["|{0:<9d} |{0:^9x} |{0:>10b}|".format(number) for number in range(1, 15)]
     result.insert(0, header)
     return result
 
