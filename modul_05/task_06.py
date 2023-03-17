f754de9517cde484d87d88839e935cf929a6434e
AleksandrRevuka <113589337+AleksandrRevuka@users.noreply.github.com>
2023-03-17 21:34:06 +0200

    task_07

--- modul_05/task_06.py
+++ modul_05/task_06.py
@@ -1,39 +1,39 @@
-# def is_spam_words(text, spam_words, space_around=False):
-#   """My_1"""
-#     for spam_word in spam_words:
-#         if spam_word.lower() in text.lower():
-#             if space_around is False:
-#                 return True
-#             elif space_around is True:
-#                 for word in text.split(' '):
-#                     word = word.strip('., ,,')
-#                     if word == spam_word:
-#                         return True
-#                     else:
-#                         continue
-#
-#     return False
+def is_spam_words(text, spam_words, space_around=False):
+    """My_1"""
+    for spam_word in spam_words:
+        if spam_word.lower() in text.lower():
+            if space_around is False:
+                return True
+            elif space_around is True:
+                for word in text.split(' '):
+                    word = word.strip('., ,,')
+                    if word == spam_word:
+                        return True
+                    else:
+                        continue
+
+    return False
 
 
 ##################################################################################################
 
-import re
+# import re
 
 
-def is_spam_words(text, spam_words, space_around=False):
-    """GPT_chat"""
-    text = text.lower()
+# def is_spam_words(text, spam_words, space_around=False):
+#     """GPT_chat"""
+#     text = text.lower()
 
-    for spam_word in spam_words:
-        if space_around:
-            pattern = r'(^|\s)' + re.escape(spam_word) + r'($|\s|\.\,)'
-            if re.search(pattern, text):
-                return True
-        else:
-            if spam_word in text:
-                return True
+#     for spam_word in spam_words:
+#         if space_around:
+#             pattern = r'(^|\s)' + re.escape(spam_word) + r'($|\s|\.\,)'
+#             if re.search(pattern, text):
+#                 return True
+#         else:
+#             if spam_word in text:
+#                 return True
 
-    return False
+#     return False
 
 ##################################################################################################
 
--- modul_05/task_07.py
+++ modul_05/task_07.py
@@ -1,8 +1,30 @@
-CYRILLIC_SYMBOLS = [ord(symbol) for symbol in "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"]
+# CYRILLIC_SYMBOLS = [ord(symbol) for symbol in "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"]
+# TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
+#                "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g")
+
+# TRANS = dict(zip(CYRILLIC_SYMBOLS, TRANSLATION))
+
+
+# def translate(name):
+#     return name.translate(TRANS)
+
+
+# print(translate("ДДДДДдддмитро Короб"))  # Dmitro Korob
+# print(translate("Олекса Івасюк"))  # Oleksa Ivasyuk
+
+
+##############################################################################################
+
+CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
 TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
                "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g")
 
-TRANS = dict(zip(CYRILLIC_SYMBOLS, TRANSLATION))
+print(zip(CYRILLIC_SYMBOLS, TRANSLATION))
+
+TRANS = {}
+for c, l in zip(CYRILLIC_SYMBOLS, TRANSLATION):
+    TRANS[ord(c)] = l
+    TRANS[ord(c.upper())] = l.upper()
 
 
 def translate(name):
@@ -11,19 +33,3 @@ def translate(name):
 
 print(translate("Дмитро Короб"))  # Dmitro Korob
 print(translate("Олекса Івасюк"))  # Oleksa Ivasyuk
-
-
-##############################################################################################
-
-# CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
-# TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
-#                "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g")
-#
-# TRANS = {}
-# for c, l in zip(CYRILLIC_SYMBOLS, TRANSLATION):
-#     TRANS[ord(c)] = l
-#     TRANS[ord(c.upper())] = l.upper()
-#
-#
-# def translate(name):
-#     return name.translate(TRANS)
