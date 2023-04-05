7efa3728d1ad275ff95d1069dd877c286f9eeb4d
Insight <Asmodians@hotmail.com>
2023-04-05 11:48:54 +0300

    HW_08 finish

--- modul_08/HW_08_birthday.py
+++ modul_08/HW_08_birthday.py
@@ -1,11 +1,16 @@
+"""
+You need to implement a useful function to display a list of colleagues who need to be 
+congratulated on their birthdays for the week.
+"""
 
+from typing import List, Dict, Tuple
 from datetime import datetime, timedelta
 from prettytable import PrettyTable
 
 
 USERS = [
     {"name": "Андрій", "birthday": datetime(1990, 4, 1)},
-    {"name": "Олександр", "birthday": datetime(1991, 4,2)},
+    {"name": "Олександр", "birthday": datetime(1991, 4, 2)},
     {"name": "Ірина", "birthday": datetime(1992, 4, 3)},
     {"name": "Марія", "birthday": datetime(1989, 4, 4)},
     {"name": "Володимир", "birthday": datetime(1987, 4, 5)},
@@ -27,23 +32,56 @@ USERS = [
 ]
 
 
-def get_birthdays_per_week(users):
+def get_date_next_week() -> Tuple[List[str], str]:
     """
-    Prints a table of the birthdays for each day of the week. 
-    Users whose birthdays fall on a weekend are listed on the following Monday.
+    Returns a list of dates representing the next week's dates, as well as the current year.
+    Each date is in the format '14 May', representing the day of the month and the month's name.
+    """
+    date_week = []
+    current_date = datetime.now().date()
+    current_year = str(current_date.year)
+
+    for day in range(7):
+        date = current_date + timedelta(days=day)
+        date = date.strftime('%d %B')
+        date_week.append(date)
+
+    return date_week, current_year
+
+
+def print_table_with_user_birthday(users_birthday: Dict[str, list], days_of_week: List[str]):
+    """
+    The function sorts the users by birthday date and prints the pretty table of birthdays for the current week.
+    """
+    today = datetime.today().weekday()
+
+    sorted_users_birthday = {}
+    for i, day in enumerate(days_of_week[today:] + days_of_week[:today]):
+        sorted_users_birthday[day] = users_birthday[day]
+        if i == 4:
+            break
+
+    table = PrettyTable()
+    table.field_names = ["Day of Week", "Birthdays 🎉 🎂 🎁"]
+
+    for day, birthdays in sorted_users_birthday.items():
+        table.add_row([day, f"{', '.join(birthdays)}" if birthdays else "-"])
+
+    print(table)
+
+
+def get_birthdays_per_week(users: List[Dict[str, str | datetime]]):
+    """
+    Prints a table with the birthdays for the upcoming week. The function receives a list of dictionaries 
+    containing users information. For each user, the function checks if their birthday is in the next week 
+    and adds them to the corresponding day of the week in a dictionary. The dictionary is then passed to 
+    the print_table_with_user_birthday function for printing.
     """
     weekend_days = ["Saturday", "Sunday"]
     days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
     users_birthday = {day: [] for day in days_of_week}
 
-    date_week = []
-    current_date = datetime.now().date()
-    current_year = str(current_date.year)
-
-    for day in range(0, 7):
-        date = current_date + timedelta(days=day)
-        date = date.strftime('%d %B')
-        date_week.append(date)
+    date_week, current_year = get_date_next_week()
 
     for user in users:
         user_date = user['birthday'].strftime('%d %B')
@@ -59,13 +97,7 @@ def get_birthdays_per_week(users):
             else:
                 users_birthday[day_of_week].append(name)
 
-    table = PrettyTable()
-    table.field_names = ["Day of Week", "Birthdays 🎉 🎂 🎁"]
-
-    for day, birthdays in users_birthday.items():
-        table.add_row([day, f"{', '.join(birthdays)}" if birthdays else "-"])
-
-    print(table)
+    print_table_with_user_birthday(users_birthday, days_of_week)
 
 
 if __name__ == '__main__':
