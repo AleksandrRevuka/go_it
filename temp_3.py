9e4ca732cae1e37699e8662738107796160c768f
Insight <Asmodians@hotmail.com>
2023-04-07 20:47:03 +0300

    task_01

--- temp_3.py
+++ temp_3.py
@@ -46,13 +46,56 @@
 # print(counts_trougth('uuuddddduudduuududduduuduudududd'))
 
 
-scores = [66, 76, 87, 45, 98, 78, 90, 45, 92, 67]
+# scores = [66, 76, 87, 45, 98, 78, 90, 45, 92, 67]
 
-over_75 = list(filter(lambda score: score > 75, scores))
+# over_75 = list(filter(lambda score: score > 75, scores))
 
-print(over_75)
+# print(over_75)
 
-print(list(map(lambda x: x**2, scores)))
+# print(list(map(lambda x: x**2, scores)))
 
-fac = lambda x: x * fac(x - 1) if x else 1
-print(fac(10))
+# fac = lambda x: x * fac(x - 1) if x else 1
+# print(fac(10))
+
+
+# def f_func(func):
+#     """..."""
+#     def f1_func(a, b):
+#         print("hello")
+#         if b == 0:
+#             return 'No'
+#         return func(a, b)
+#     return f1_func
+
+
+# @f_func
+# def f2_func(a, b):
+#     """..."""
+#     return a % b
+
+# print(f2_func(10, 5))
+
+
+def complicated(x, y):
+    return x / y
+
+
+def logged_func(func):
+    def inner(x, y):
+        print(f'called with {x}, {y}')
+        result = func(x, y)
+        print(f'result: {result}')
+        return result
+    return inner
+
+
+complicated = logged_func(complicated)
+b = complicated(10, 5)
+
+
+OPERATIONS = {
+    '-': lambda x, y: x - y,
+    '+': lambda x, y: x + y,
+    '*': lambda x, y: x * y,
+    '/': lambda x, y: x / y,
+}
