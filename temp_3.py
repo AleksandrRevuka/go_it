882d50c011946edcee515f96019f9f8ab7b7e291
Insight <Asmodians@hotmail.com>
2023-04-07 09:21:01 +0300

    temp_3

--- temp_3.py
+++ temp_3.py
@@ -1,19 +1,58 @@
-import sys
+# from collections import Counter
 
-from temp_1 import main as greeting
-from temp_2 import main as exiting
+# def get_two_biggest(data: dict):
+#     values = list(data.values())
+#     keys = list(data.keys())
+#     largest_values = sorted(values, reverse=True)[:2]
+
+#     return [key for key in keys if data[key] in largest_values]
+
+# data = {
+#     'a': 34,
+#     'b': 66,
+#     'c': 12,
+#     'd': 9000,
+# }
+
+# print(get_two_biggest(data))
 
 
-def main():
-    try:
-        if sys.argv[1] == 'greet':
-            greeting()
-        elif sys.argv[1] == 'goodbuy':
-            exiting()
-        else:
-            print("Unknown argument")
-    except IndexError:
-        print("Argument must be 'greet' or 'goodbuy'")
+# def counts_trougth(data: str):
+    # way_value = 0
+    # count_way = []
+    # down = 'd'
+    # up = 'u'
+    # for move in data:
+    #     if move == up:
+    #         way_value += 1
+    #         count_way.append(str(way_value) + up)
+    #     elif move == down:
+    #         way_value -= 1
+    #         count_way.append(str(way_value) + down)
+    # print(count_way)
+    # result = Counter(count_way)
+    # return result['-1d']
 
-if __name__ == '__main__':
-    main()
+#     summ = count = 0
+#     for s in data:
+#         if s == 'd':
+#             summ -= 1
+#         elif s == 'u':
+#             summ += 1
+#         if summ == 0 and s == 'd':
+#             count += 1
+#     return count
+
+# print(counts_trougth('uuuddddduudduuududduduuduudududd'))
+
+
+scores = [66, 76, 87, 45, 98, 78, 90, 45, 92, 67]
+
+over_75 = list(filter(lambda score: score > 75, scores))
+
+print(over_75)
+
+print(list(map(lambda x: x**2, scores)))
+
+fac = lambda x: x * fac(x - 1) if x else 1
+print(fac(10))
