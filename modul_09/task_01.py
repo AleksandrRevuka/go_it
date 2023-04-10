2bacfd06712888907557230f2cd88c6a81940c09
Insight <Asmodians@hotmail.com>
2023-04-10 18:35:24 +0300

    task_02

--- modul_09/task_01.py
+++ modul_09/task_01.py
@@ -1,9 +1,22 @@
+"""
+Минулого разу ми реалізували дві функції. Перша - get_grade, приймає ключ в оцінці ECTS і 
+повертає відповідну п'ятибальну оцінку (перший стовпчик таблиці). Друга - get_description, теж 
+приймає ключ у оцінки ECTS, але повертає пояснення оцінки у текстовому форматі (останній стовпчик 
+таблиці). На неіснуючий ключ функції повинні повертати значення None.
+
+Реалізуйте функцію вищого порядку get_student_grade, яка приймає параметр option. Якщо він 
+дорівнює значенням "grade", то функція повертає функцію get_grade, а якщо його значення дорівнює 
+"description", то повертає функцію get_description. Якщо параметр за значенням не співпав із 
+заданими, то функція get_student_grade повинна повертати значення None.
+"""
 def get_grade(key):
+    """..."""
     grade = {"A": 5, "B": 5, "C": 4, "D": 3, "E": 3, "FX": 2, "F": 1}
     return grade.get(key, None)
 
 
 def get_description(key):
+    """..."""
     description = {
         "A": "Perfectly",
         "B": "Very good",
@@ -17,7 +30,7 @@ def get_description(key):
 
 
 def get_student_grade(option: str):
-
+    """..."""
     if option == 'grade':
         return get_grade
 
