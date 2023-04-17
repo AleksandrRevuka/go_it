29866d67a0ccd081554a43c88150fb447ce9c0af
Insight <Asmodians@hotmail.com>
2023-04-17 14:28:50 +0300

    task_02

---
+++ modul_10.py/task_02.py
@@ -0,0 +1,10 @@
+class Animal:
+    def __init__(self, nickname, weight) -> None:
+        self.nickname = nickname
+        self.weight = weight
+
+    def say(self):
+        pass
+
+
+animal = Animal('Bob', 23)
